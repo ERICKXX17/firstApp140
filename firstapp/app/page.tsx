fd70a6f3756e0e97361ea1a3a6ad5b69dc13dd9b
ERICKXX17 <ep509122@gmail.com>
2024-02-13 20:14:34 -0600

    first commit

---
+++ firstapp/app/page.tsx
@@ -0,0 +1,28 @@
+import Image from "next/image";
+import styles from "@/public/styles/index.module.css";
+
+export default function Home() {
+ return(
+
+  <main>
+
+
+<div className={styles.container}>
+<nav className={styles.navbar}>
+
+
+<a href="#">Home</a>
+<a href="#">Acerca</a>
+
+</nav>
+
+<body>
+
+
+
+</body>
+</div>
+
+  </main>
+ )
+}
